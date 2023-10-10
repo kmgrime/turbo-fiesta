e5d8a14f1986a10511bca9913c157aca1b3507ee
kmgrime <kmgrime@gmail.com>
2023-10-10 13:24:48 +0200

    add filesorting script

---
+++ scripts/filesorting.py
@@ -0,0 +1,53 @@
+# imports
+import os
+import glob
+import shutil
+from os import path
+
+# what files with which extensions to move
+filename=glob.glob("C:/Users/Aleti Sunil/Downloads/*")
+documents=['.pdf','.docx','.doc','.txt']
+media=['.jpeg','.jpg','.svg','.png','.PNG','.mp4','.mp3']
+setupFiles=['.exe','.msi']
+compressedFiles=['.zip']
+files=['.apk']
+
+# file locations
+DocumentsLocation='C:/Users/Aleti Sunil/Downloads/documents'
+mediaLocation='C:/Users/Aleti Sunil/Downloads/media'
+setupFilesLocation='C:/Users/Aleti Sunil/Downloads/setupFiles'
+compressedFilesLocation='C:/Users/Aleti Sunil/Downloads/compressedFiles'
+FilesLocation='C:/Users/Aleti Sunil/Downloads/Files'
+
+# script for moving the files corresponding to the extensions
+for file in filename:
+    if os.path.splitext(file)[1] in documents:
+        if(path.exists(DocumentsLocation)):
+            shutil.move(file,DocumentsLocation)
+        else:
+            os.mkdir(DocumentsLocation)
+            shutil.move(file,DocumentsLocation)
+    if os.path.splitext(file)[1] in media:
+        if(path.exists(mediaLocation)):
+            shutil.move(file,mediaLocation)
+        else:
+            os.mkdir(mediaLocation)
+            shutil.move(file,mediaLocation)
+    if os.path.splitext(file)[1] in setupFiles:
+        if(path.exists(setupFilesLocation)):
+            shutil.move(file,setupFilesLocation)
+        else:
+            os.mkdir(setupFilesLocation)
+            shutil.move(file,setupFilesLocation)
+    if os.path.splitext(file)[1] in compressedFiles:
+        if(path.exists(compressedFilesLocation)):
+            shutil.move(file,compressedFilesLocation)
+        else:
+            os.mkdir(compressedFilesLocation)
+            shutil.move(file,compressedFilesLocation)
+    if os.path.splitext(file)[1] in files:
+        if(path.exists(FilesLocation)):
+            shutil.move(file,FilesLocation)
+        else:
+            os.mkdir(FilesLocation)
+            shutil.move(file,FilesLocation)
